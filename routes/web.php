007f58d54d376c8c4ae399615cdc64bb46768dda
CautiousCoder <cautious.coder0@gmail.com>
2022-04-22 22:50:13 +0600

    Admin panel setup and category add

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+
+Route::group(['prefix' => 'admin'], function(){
+    Route::get('/', function () {
+        return view('backend.admin.starter');
+    });
+
+    Route::resource('category', CategoryController::class);
 });
