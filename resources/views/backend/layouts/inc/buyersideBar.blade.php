d136592005181ccf006820541d588dfacfb57a44
CautiousCoder <cautious.coder0@gmail.com>
2022-06-09 15:58:58 +0600

    sidebar name change

--- resources/views/backend/layouts/inc/buyersideBar.blade.php
+++ resources/views/backend/layouts/inc/buyersideBar.blade.php
@@ -1,9 +1,9 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-        <img src="{{ asset('backEnd') }}/dist/img/AdminLTELogo.png" alt="AdminLTE Logo"
+        <img src="{{ asset('backEnd') }}/dist/img/AdminLTELogo.png" alt="Auction Logo"
             class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">AdminLTE 3</span>
+        <span class="brand-text font-weight-light">Online Auction</span>
     </a>
 
     <!-- Sidebar -->
