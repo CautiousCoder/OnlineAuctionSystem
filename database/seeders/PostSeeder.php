583bde975ce61afd7cd2ef9b12bfe4e632e23466
CautiousCoder <cautious.coder0@gmail.com>
2022-06-15 18:20:08 +0600

    post seeder work done

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -17,34 +17,10 @@ class PostSeeder extends Seeder
      */
     public function run()
     {
-        // $category = Category::all()->random(3);
-        // $tags = Tag::all()->random(4);
-        // Post::all()->each(function ($user) use ($category) {
-        //     $user->roles()->attach(
-        //         $category->random(3)->pluck('id')->toArray()
-        //     );
-        // });
-        // Post::all()->each(function ($user) use ($tags) {
-        //     $user->roles()->attach(
-        //         $tags->random(4)->pluck('id')->toArray()
-        //     );
-        // });
-
         Category::factory()->count(10)->create();
         Tag::factory()->count(10)->create();
         $category = Category::all()->random(3);
         $tag = Tag::all()->random(4);
-
-        // Post::factory()
-        //     ->hasAttached(
-        //         Category::factory()
-        //             ->count(3)
-        //             ->state(function (array $attributes, Post $post) {
-        //                 return ['title' => $post->title . ' Category'];
-        //             }),
-        //         ['active' => true]
-        //     )
-        //     ->create();
         Post::factory()
             ->count(3)
             ->hasAttached($category, ['category_id' => 1, 'post_id' => 1])
